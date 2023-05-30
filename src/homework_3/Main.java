a64fdf0b85ea0b1898eea749c35ab5fc92799f4e
vlad-danylenko <vlad.danilenko97@gmail.com>
2023-05-30 23:14:33 +0300

    homework 3

--- src/homework_3/Main.java
+++ src/homework_3/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Integer a = 6, b = 2;
 
-        System.out.println(a+b); // Result 8
-        System.out.println(a-b); // Result 4
-        System.out.println(a/b); // Result 3
-        System.out.println(a*b); // Result 12
+        System.out.println("Результат додавання: " + (a+b)); // Result 8
+        System.out.println("Результат віднімання: " + (a-b)); // Result 4
+        System.out.println("Результат ділення: " + (a/b)); // Result 3
+        System.out.println("Результат множення: " + (a*b)); // Result 12
     }
 }
