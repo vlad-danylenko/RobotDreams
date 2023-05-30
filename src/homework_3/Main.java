7b2f36a4abd2f60b55ed82fd59ad0fec1cbce538
vlad-danylenko <vlad.danilenko97@gmail.com>
2023-05-30 23:06:44 +0300

    main

---
+++ src/homework_3/Main.java
@@ -0,0 +1,12 @@
+package src.homework_3;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer a = 6, b = 2;
+
+        System.out.println(a+b); // Result 8
+        System.out.println(a-b); // Result 4
+        System.out.println(a/b); // Result 3
+        System.out.println(a*b); // Result 12
+    }
+}
