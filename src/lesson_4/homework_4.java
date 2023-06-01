8cfb448b4cc9c4788ecd1ce25f3ba4ccb58a552f
vlad-danylenko <vlad.danilenko97@gmail.com>
2023-06-01 21:22:09 +0300

    lesson 4

--- src/lesson_4/homework_4.java
+++ src/lesson_4/homework_4.java
@@ -2,12 +2,6 @@ package src.lesson_4;
 
 public class homework_4 {
     public static void main(String[] args) {
-        String myVariable = "RobotDreams";
-        System.out.println(myVariable.toUpperCase()); // ROBOTDREAMS
-        System.out.println(myVariable.toLowerCase()); // robotdreams
-
-        System.out.println(myVariable.charAt(0)); // R
-        System.out.println(myVariable.charAt(myVariable.length()-1)); // s
 
     }
 
