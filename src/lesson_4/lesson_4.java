fa5f40712037dea2ba4612baf6acb080e7242ed9
vlad-danylenko <vlad.danilenko97@gmail.com>
2023-06-01 21:16:28 +0300

    lesson 4

---
+++ src/lesson_4/lesson_4.java
@@ -0,0 +1,14 @@
+package src.lesson_4;
+
+public class lesson_4 {
+    public static void main(String[] args) {
+        lesson_4 lesson = new lesson_4();
+        int result = lesson.sum(2, 3);
+        System.out.println(result);
+
+    }
+    private static int sum (int a, int b) {
+        return a+b;
+    }
+}
+
