0b16d75213199abc7d33d9cbe1278e3cc6d96b20
vlad-danylenko <vlad.danilenko97@gmail.com>
2023-06-15 20:52:20 +0300

    practice

--- src/lesson_7/practice.java
+++ src/lesson_7/practice.java
@@ -12,18 +12,20 @@ public class practice {
         System.out.println("Напишіть цифрою яку операцію необхідно виконати? \n1 - Відняти\n2 - Додати\n3 - Помжножити\n4 - Поділити");
         int operationFromUser = reader.nextInt();
 
-        System.out.println("Результат: ");
-        defineOperation(number1,number2,operationFromUser);
+
+        System.out.println("Результат: " + defineOperation(number1,number2,operationFromUser));
+
     }
 
 
-    private static void defineOperation (int number1, int number2, int operationFromUser) {
-        switch (operationFromUser) {
-            case 1 -> System.out.println(minus(number1,number2));
-            case 2 -> System.out.println(plus(number1,number2));
-            case 3 -> System.out.println(multiply(number1,number2));
-            case 4 -> System.out.println(divide(number1,number2));
-        };
+    private static int defineOperation (int number1, int number2, int operationFromUser) {
+        int result = switch (operationFromUser) {
+            case 1 -> minus(number1,number2);
+            case 2 -> plus(number1,number2);
+            case 3 -> multiply(number1,number2);
+            case 4 -> divide(number1,number2);
+            default -> throw new IllegalStateException("Unexpected value: " + operationFromUser);
+        }; return result;
     }
     private static int minus (int number1, int number2) {
         return number1-number2;
