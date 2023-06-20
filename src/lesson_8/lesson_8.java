2f51aaf26fe76234a44b2399bf33a7e90517883b
vlad-danylenko <vlad.danilenko97@gmail.com>
2023-06-20 12:52:00 +0300

    lesson 8

---
+++ src/lesson_8/lesson_8.java
@@ -0,0 +1,15 @@
+package src.lesson_8;
+
+import java.util.Random;
+
+
+public class lesson_8 {
+
+    private final static int range = 100;
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+    }
+}
