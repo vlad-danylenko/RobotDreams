872d78299d51272779f80e4ac77eb12e08319b80
vlad-danylenko <vlad.danilenko97@gmail.com>
2023-06-30 15:42:25 +0300

    homework from lesson 11

---
+++ src/lesson_11/Main.java
@@ -0,0 +1,12 @@
+package src.lesson_11;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Truck truck = new Truck();
+
+        car.carType();
+        truck.carType();
+
+    }
+}
